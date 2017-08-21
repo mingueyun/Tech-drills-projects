742f1576b25f14be9c47ebbfa10cf8a38e6133f4
mingueyun <mingueyun@gmail.com>
2017-08-20 19:15:54 -0700

    upload
    simple calc from cs dev u lesson 2

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/SimpleCalculator.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SimpleCalculator.aspx.cs" Inherits="ChallengeSimpleCalculator.SimpleCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="X-Large" Text="Simple Calculator"></asp:Label>
+            <br />
+            <br />
+            First Value:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="firstTextBox" runat="server" Width="75px"></asp:TextBox>
+            <br />
+            <br />
+            Second value:
+            <asp:TextBox ID="secondTextBox" runat="server" Width="75px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" BorderStyle="Solid" OnClick="addButton_Click" Text="+" Width="40px" />
+            <asp:Button ID="subtractButton" runat="server" BorderStyle="Solid" OnClick="subtractButton_Click" Text="-" Width="40px" />
+            <asp:Button ID="multiplyButton" runat="server" BorderStyle="Solid" OnClick="multiplyButton_Click" Text="*" Width="40px" />
+            <asp:Button ID="divideButton" runat="server" BorderStyle="Solid" OnClick="divideButton_Click" Text="/" Width="40px" />
+            <br />
+            <br />
+            <asp:Label ID="totalLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/SimpleCalculator.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class SimpleCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string One = firstTextBox.Text;
+            string Two = secondTextBox.Text;
+
+            totalLabel.Text = (Convert.ToDouble(firstTextBox.Text) + Convert.ToDouble(secondTextBox.Text)).ToString();
+
+//          this code had issues with not passing runtime which I could not do fractions
+//          string One = firstTextBox.Text;
+//          string Two = secondTextBox.Text;
+//          int valueOne = int.Parse(One);
+//          int valueTwo = int.Parse(Two);
+//          totalLabel.Text = (Convert.ToDouble(valueOne) + (valueTwo)).ToString();
+
+//          example of an if statement for an operand that returns true  and exception but i am not sure what differnce this made but it still
+//          works. 
+//          if (!string.IsNullOrEmpty(firstTextBox.Text) && !string.IsNullOrEmpty(secondTextBox.Text))
+//          totalLabel.Text = (Convert.ToDouble(firstTextBox.Text) - Convert.ToDouble(secondTextBox.Text)).ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            string One = firstTextBox.Text;
+            string Two = secondTextBox.Text;
+
+            totalLabel.Text = (Convert.ToDouble(firstTextBox.Text) - Convert.ToDouble(secondTextBox.Text)).ToString();
+
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            string One = firstTextBox.Text;
+            string Two = secondTextBox.Text;
+
+            totalLabel.Text = (Convert.ToDouble(firstTextBox.Text) * Convert.ToDouble(secondTextBox.Text)).ToString();
+
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            string One = firstTextBox.Text;
+            string Two = secondTextBox.Text;
+
+            totalLabel.Text = (Convert.ToDouble(firstTextBox.Text) / Convert.ToDouble(secondTextBox.Text)).ToString();
+
+        }
+    }
+}
