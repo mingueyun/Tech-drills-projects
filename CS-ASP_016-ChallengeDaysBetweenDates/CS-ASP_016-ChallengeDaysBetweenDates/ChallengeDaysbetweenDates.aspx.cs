26d7ed8a78eba31a6434814ebe33fd72edc76ba8
mingueyun <mingueyun@gmail.com>
2017-08-27 21:33:30 -0700

    update to code
    Added statements that give date output for each calender before it compares the dates bwtween each calender

--- CS-ASP_016-ChallengeDaysBetweenDates/CS-ASP_016-ChallengeDaysBetweenDates/ChallengeDaysbetweenDates.aspx.cs
+++ CS-ASP_016-ChallengeDaysBetweenDates/CS-ASP_016-ChallengeDaysBetweenDates/ChallengeDaysbetweenDates.aspx.cs
@@ -11,44 +11,46 @@ namespace CS_ASP_016_ChallengeDaysBetweenDates
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Label1.Text = "";
         }
 
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
+            
+            foreach (DateTime day in Calendar1.SelectedDates)
+            {
+                Label1.Text = day.Date.ToShortDateString();
+            }
 
-        }
+            foreach (DateTime day in Calendar2.SelectedDates)
+            {
+                Label1.Text = day.Date.ToShortDateString();
+            }
+
+
+        } 
 
         protected void Button1_Click(object sender, EventArgs e)
+
         {
+             Label1.Text = "";
+
+            
+
             if (Calendar1.SelectedDate > Calendar2.SelectedDate)
             {
-                Label1.Text = Calendar1.SelectedDate.Subtract(Calendar2.SelectedDate).TotalDays.ToString();
+                Label1.Text = Label1.Text = Calendar1.SelectedDate.Subtract(Calendar2.SelectedDate).TotalDays.ToString();
             }
-
+            else if (Calendar1.SelectedDate < Calendar2.SelectedDate)
+            {
+                Label1.Text = Label1.Text = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate).TotalDays.ToString();
+            }
             else
             {
-                Label1.Text = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate).TotalDays.ToString();
+
             }
-
-            /*
-
-                                    DateTime firstDate = Calendar1.SelectedDate;
-                                    DateTime secondDate = Calendar2.SelectedDate;
-
-                                    TimeSpan differenceInDays;
-
-                                    if (firstDate > secondDate)
-                                        differenceInDays = (firstDate - secondDate);
-                                    else
-                                        differenceInDays = (secondDate - firstDate);
-
-                                    int did = differenceInDays.Days;
-                                    Label1.Text = did.ToString();
-            */
-
-
+                
 
         }
-    }
+    }   
 }
