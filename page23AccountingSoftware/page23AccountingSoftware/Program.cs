dc3d8540ab2a3462816a7a27722352fa91804eda
mingueyun <mingueyun@gmail.com>
2018-06-10 05:02:03 -0700

    page 23
    New c sharp page 23

---
+++ page23AccountingSoftware/page23AccountingSoftware/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page23AccountingSoftware
+{
+    class Program
+    {
+        static void Main()
+        {
+          
+            string second = "Remember, we're \"accounting\" on you! ";
+
+            Console.WriteLine("Welcome to Acme Accounting Systems \n"  + second);
+            Console.ReadLine();
+        }
+    }
+}
