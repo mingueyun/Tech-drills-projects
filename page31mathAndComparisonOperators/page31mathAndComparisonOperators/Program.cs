698de5d1f3a486539da23330d39160643c265e63
mingueyun <mingueyun@gmail.com>
2018-06-17 04:00:13 -0700

    added page 31 exercise
    Added c sharp page 31 math exercise.

---
+++ page31mathAndComparisonOperators/page31mathAndComparisonOperators/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page31mathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            //Console.WriteLine("give me a number");
+            //string bNumber = Console.ReadLine();
+            //decimal aNumber = Convert.ToDecimal(bNumber);
+            //decimal multiply = aNumber * 50;
+            //Console.WriteLine(multiply.ToString("N3"));
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("give me a number");
+            //string number = Console.ReadLine();
+            //int add = Convert.ToInt32(number);
+            //int total = add + 25;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //Console.WriteLine("give me a number");
+            //string dNumber = Console.ReadLine();
+            //double divide = Convert.ToDouble(dNumber);
+            //double fTotal = divide / 12.5;
+            //Console.WriteLine(fTotal);
+            //Console.ReadLine();
+
+            //Console.WriteLine("give me a number");
+            //string hNumber = Console.ReadLine();
+            //double greaterThen = Convert.ToDouble(hNumber);
+            //bool gThen = greaterThen > 50;
+            //Console.WriteLine(g);
+            //Console.ReadLine();
+
+            Console.WriteLine("give me a number");
+            string jNumber = Console.ReadLine();
+            double remainder = Convert.ToDouble(jNumber);
+            double nRemainder = remainder % 7;
+            Console.WriteLine(nRemainder);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
