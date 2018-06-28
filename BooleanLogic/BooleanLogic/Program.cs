8687888faca8ce61252eea022dbf116d3a7fe7a5
mingueyun <mingueyun@gmail.com>
2018-06-28 05:20:13 -0700

    exercise boolean logic
    added page 37 c sharp for boolean logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int nAge = Convert.ToInt32(age);
+
+            Console.WriteLine("have you ever had a dui? answer in false or true");
+            string dui = Console.ReadLine();
+            bool yesOrNo = dui == "true";
+
+
+
+            Console.WriteLine("how many speeding tickets do you have?");
+            string ticket = Console.ReadLine();
+            int nticket = Convert.ToInt32(ticket);
+
+
+
+            bool qual = nAge >= 15 && yesOrNo == false && nticket < 3 ?
+
+            true : false;
+            Console.WriteLine("Qualified?\n" + qual);
+            Console.ReadLine();
+
+        }
+    }
+}
